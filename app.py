2707559486cea9708707449e1b00f04ecd7dcf06
shakilahmmeed <shakilahmmeed@gmail.com>
2020-06-18 06:20:29 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
+
+@app.route('/about')
+def about():
+    return 'This is about page'
